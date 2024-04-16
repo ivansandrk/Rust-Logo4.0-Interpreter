66a366347fa8447d72cef09688cd0aa05d612671
Ivan Šandrk <ivan.sandrk@gmail.com>
2024-04-16 22:27:21 +0200

    Last uncommited changes, not sure if they should work

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@ use std::io;
 use std::sync::mpsc;
 use std::thread;
 use std::time;
-use std::rc::Rc;
-use std::cell::RefCell;
 
 use ggez::{conf, Context, ContextBuilder};
 use ggez::event;
@@ -25,6 +23,14 @@ const HEIGHT: u32 = 400;
 const COLOR: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 const INJECTED_COMMANDS: &[&str] = &[
   "repeat 6 [fd 50 lt 120 repeat 6 [fd 10 rt 60] rt 120 rt 60]"
+  // "repeat 6 [ fd 20 rt 60 ]"
+  // "fd 50",
+  // "lt 90",
+  // "fd 30",
+  // "rt 90",
+  // "fd 30",
+  // "lt 90",
+  // "fd 20"
 ];
 
 fn get_input_receiver() -> mpsc::Receiver<String> {
@@ -52,12 +58,12 @@ fn get_input_receiver() -> mpsc::Receiver<String> {
 }
 
 struct MainState {
-  context: Rc<RefCell<Context>>,
-  canvas: Rc<RefCell<graphics::Canvas>>,
+  context: Context,
   continuing: bool,
   events: event::Events,
   receiver: mpsc::Receiver<String>,
-  evaluator: evaluator::Evaluator,
+  canvas: graphics::Canvas,
+  turtle: Option<turtle::Turtle>,
 }
 
 impl MainState {
@@ -70,37 +76,64 @@ impl MainState {
     graphics::clear(&mut context);
     graphics::set_canvas(&mut context, None);
 
-    let context = Rc::new(RefCell::new(context));
-    let canvas = Rc::new(RefCell::new(canvas));
-    let graphics = GgezGraphics::new(Rc::clone(&context), Rc::clone(&canvas));
-    let evaluator = evaluator::Evaluator::new(Box::new(graphics));
-
     Self {
       context: context,
-      canvas: canvas,
       continuing: true,
       events,
       // Receiver for stdio input.
       receiver: get_input_receiver(),
-      evaluator: evaluator,
+      canvas,
+      turtle: Some(turtle::Turtle::new()),
     }
   }
 
   fn update(&mut self) {
     if let Ok(input) = self.receiver.try_recv() {
-      self.evaluator.feed(input.as_str());
+      // TODO: evaluator.feed(input, graphics)
+      let mut parser = parser::Parser::new();
+      parser.feed(&input);
+      let commands = parser.parse_all();
+      for command in commands.iter() {
+        // Temporarily take turtle out of self so we can call exec_command,
+        // otherwise we would have self.turtle.exec_command(command, self)
+        // which would complain about mutably borring self twice.
+        let mut turtle = self.turtle.take().unwrap();
+        turtle.exec_command(command, self);
+        self.turtle = Some(turtle);
+      }
     }
   }
 
-  fn draw(&mut self) {
-    graphics::set_background_color(&mut self.context.borrow_mut(), graphics::Color::from((0, 0, 0, 255)));
-    graphics::clear(&mut self.context.borrow_mut());
+  // Needs to know screen width & height.  Returns implementation specific
+  // point type.
+  fn origin_to_screen_coords(&self, point: (f32, f32)) -> Point2 {
+    let mut width  = WIDTH as f32;
+    let mut height = HEIGHT as f32;
+    #[cfg(target_os="macos")]
+    {
+      println!("We're on Mac!");
+      width  /= 2.0;
+      height /= 2.0;
+    }
+    #[cfg(target_os="linux")]
+    println!("We're on Linux!");
 
-    // Draw canvas.
+    println!("{:?} {:?}", width, height);
+
+    let ret = Point2::new(width  + point.0,
+                          height - point.1);
+    println!("{:?}", ret);
+    ret
+  }
+
+  fn draw(&mut self) {
+    graphics::set_background_color(&mut self.context, graphics::Color::from((0, 0, 0, 255)));
+    graphics::clear(&mut self.context);
+
     // TODO: Use ? for this function.
     graphics::draw_ex(
-      &mut self.context.borrow_mut(),
-      &*self.canvas.borrow(),
+      &mut self.context,
+      &self.canvas,
       graphics::DrawParam {
         // color: Some(graphics::Color::from((255, 255, 255, 255))),
         scale: Point2::new(0.5, 0.5),
@@ -108,19 +141,16 @@ impl MainState {
       },
     ).unwrap();
 
-    // TODO: Draw the turtle.
-    // println!("{} {}", self.evaluator.turtle.x, self.evaluator.turtle.y);
-
-    graphics::present(&mut self.context.borrow_mut());
+    graphics::present(&mut self.context);
   }
 
   fn handle_events(&mut self) {
     // Tell the timer stuff a frame has happened.
     // Without this the FPS timer functions and such won't work.
-    self.context.borrow_mut().timer_context.tick();
+    self.context.timer_context.tick();
     // Handle events
     for event in self.events.poll() {
-      self.context.borrow_mut().process_event(&event);
+      self.context.process_event(&event);
       match event {
         event::Event::Quit { .. }
         | event::Event::KeyDown {
@@ -136,51 +166,20 @@ impl MainState {
   }
 }
 
-// Needs to know screen width & height.  Returns implementation specific point type.
-fn origin_to_screen_coords(point: (f32, f32)) -> Point2 {
-  let mut width  = WIDTH as f32;
-  let mut height = HEIGHT as f32;
-  #[cfg(target_os="macos")]
-  {
-    width  /= 2.0;
-    height /= 2.0;
-  }
-
-  Point2::new(width  + point.0,
-              height - point.1)
-}
-
-struct GgezGraphics {
-  context: Rc<RefCell<ggez::Context>>,
-  canvas: Rc<RefCell<graphics::Canvas>>,
-}
-
-impl GgezGraphics {
-  fn new(context: Rc<RefCell<ggez::Context>>,
-         canvas: Rc<RefCell<graphics::Canvas>>) -> GgezGraphics {
-    GgezGraphics {
-      context,
-      canvas,
-      // context: context.clone(),
-      // canvas: canvas.clone(),
-    }
-  }
-}
-
-impl evaluator::Graphics for GgezGraphics {
+impl turtle::TurtleGraphics for MainState {
   fn line(&mut self, p1: (f32, f32), p2: (f32, f32)) {
-    let p1 = origin_to_screen_coords(p1);
-    let p2 = origin_to_screen_coords(p2);
-    graphics::set_canvas(&mut self.context.borrow_mut(), Some(&self.canvas.borrow()));
+    let p1 = self.origin_to_screen_coords(p1);
+    let p2 = self.origin_to_screen_coords(p2);
+    graphics::set_canvas(&mut self.context, Some(&self.canvas));
     // TODO: Put ? on this function call at end, this do_commands function needs to return Result or Option.
-    graphics::line(&mut self.context.borrow_mut(), &[p1, p2], 1.0).unwrap();
-    graphics::set_canvas(&mut self.context.borrow_mut(), None);
+    graphics::line(&mut self.context, &[p1, p2], 1.0).unwrap();
+    graphics::set_canvas(&mut self.context, None);
   }
 
   fn clearscreen(&mut self) {
-    graphics::set_canvas(&mut self.context.borrow_mut(), Some(&self.canvas.borrow()));
-    graphics::clear(&mut self.context.borrow_mut());
-    graphics::set_canvas(&mut self.context.borrow_mut(), None);
+    graphics::set_canvas(&mut self.context, Some(&self.canvas));
+    graphics::clear(&mut self.context);
+    graphics::set_canvas(&mut self.context, None);
   }
 }
 
