775fdbf3657f8591fb27b0a5e25d1d90fa134d7d
Ivan Šandrk <ivan.sandrk@gmail.com>
2024-04-22 22:52:37 +0200

    use directives to shorten names, correct screen coordinate transform

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 #![allow(dead_code)]
 
+extern crate ggez;
+
 mod lexer;
 mod parser;
 mod turtle;
 mod evaluator;
 
-// use std::io;
-// use std::sync::mpsc;
-// use std::thread;
-// use std::time;
-// use std::rc::Rc;
-// use std::cell::RefCell;
-
-// use ggez::{conf, Context, ContextBuilder};
-// use ggez::event;
-// use ggez::graphics;
-// use ggez::mint::Point2;
+use ggez::graphics::{Canvas, Color, Mesh};
+use ggez::conf::{WindowSetup, WindowMode};
+use ggez::{Context, ContextBuilder, GameResult, GameError};
+use ggez::event;
+use ggez::glam::Vec2;
+use ggez::mint::Point2;
 
 const WIDTH:  f32 = 1000.;
 const HEIGHT: f32 = 1000.;
@@ -23,13 +20,6 @@ const COLOR: [f32; 4] = [0., 1., 0., 1.];
 const INJECTED_COMMANDS: &[&str] = &[
   "repeat 6 [fd 50 lt 120 repeat 6 [fd 10 rt 60] rt 120 rt 60]"
 ];
-
-// TO MN :A :N
-// MAKE "L []
-// REPEAT :N [FD :A RT 360 / :N MAKE "L LPUT GETXY :L]
-// FOR "I 1 :N [FOR "J 1 :N [SETXY ITEM :I :L SETXY ITEM :J :L]]
-// END
-
 
 fn get_input_receiver() -> std::sync::mpsc::Receiver<String> {
   let args: Vec<_> = std::env::args().collect();
@@ -72,35 +62,17 @@ fn get_input_receiver() -> std::sync::mpsc::Receiver<String> {
 //     graphics::present(&mut self.context.borrow_mut());
 //   }
 
-// // Needs to know screen width & height.  Returns implementation specific point type.
-// fn origin_to_screen_coords(point: (f32, f32)) -> Point2 {
-//   let mut width  = WIDTH as f32;
-//   let mut height = HEIGHT as f32;
-//   #[cfg(target_os="macos")]
-//   {
-//     width  /= 2.0;
-//     height /= 2.0;
-//   }
-//   Point2::new(width  + point.0,
-//               height - point.1)
-// }
-
-// struct GgezGraphics {
-//   context: Rc<RefCell<ggez::Context>>,
-//   canvas: Rc<RefCell<graphics::Canvas>>,
-// }
-
-// impl GgezGraphics {
-//   fn new(context: Rc<RefCell<ggez::Context>>,
-//          canvas: Rc<RefCell<graphics::Canvas>>) -> GgezGraphics {
-//     GgezGraphics {
-//       context,
-//       canvas,
-//       // context: context.clone(),
-//       // canvas: canvas.clone(),
-//     }
-//   }
-// }
+fn origin_to_screen_coords(point: (f32, f32)) -> Vec2 {
+  let mut width  = WIDTH;
+  let mut height = HEIGHT;
+  #[cfg(target_os="macos")]
+  {
+    width  /= 2.0;
+    height /= 2.0;
+  }
+  Vec2::new(width  + point.0,
+            height - point.1)
+}
 
 // impl evaluator::Graphics for GgezGraphics {
 //   fn line(&mut self, p1: (f32, f32), p2: (f32, f32)) {
@@ -120,17 +92,17 @@ fn get_input_receiver() -> std::sync::mpsc::Receiver<String> {
 // }
 
 struct MainState {
-  canvas: ggez::graphics::Canvas,
+  canvas: Canvas,
   receiver: std::sync::mpsc::Receiver<String>,
   graphics: turtle::GraphicsStub,
   evaluator: evaluator::Evaluator,
 }
 
 impl MainState {
-  fn new(ctx: &ggez::Context) -> ggez::GameResult<Self> {
-    let canvas = ggez::graphics::Canvas::from_frame(
+  fn new(ctx: &Context) -> GameResult<Self> {
+    let canvas = Canvas::from_frame(
       ctx,
-      ggez::graphics::Color::from([1., 1., 1., 1.]));
+      Color::from([1., 1., 1., 1.]));
     // graphics::set_canvas(&mut context, Some(&canvas));
     // graphics::set_background_color(&mut context, graphics::Color::from((0, 0, 0, 255)));
     // graphics::clear(&mut context);
@@ -149,28 +121,29 @@ impl MainState {
   }
 }
 
-impl ggez::event::EventHandler<ggez::GameError> for MainState {
-  fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
+impl event::EventHandler<GameError> for MainState {
+  fn update(&mut self, _ctx: &mut Context) -> GameResult {
     if let Ok(input) = self.receiver.try_recv() {
       self.evaluator.feed(&input);
     }
     Ok(())
   }
-  fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+  fn draw(&mut self, ctx: &mut Context) -> GameResult {
     // let invocations = self.graphics.invocations.replace(Vec::new());
-    let mut canvas = ggez::graphics::Canvas::from_frame(
+    let mut canvas = Canvas::from_frame(
       ctx,
-      ggez::graphics::Color::from([0., 0., 0., 1.]));
+      Color::from([0., 0., 0., 1.]));
     for cmd in self.graphics.invocations.borrow().iter() {
       match cmd {
         turtle::Command::Line(p1, p2) => {
-          let line = ggez::graphics::Mesh::new_line(
+          let line = Mesh::new_line(
             ctx,
-            &[ggez::glam::Vec2::new(p1.0, p1.1), ggez::glam::Vec2::new(p2.0, p2.1)],
+            &[origin_to_screen_coords(*p1), origin_to_screen_coords(*p2)],
             1.,
             [1., 1., 1., 1.].into()
           )?;
-          canvas.draw(&line, ggez::glam::Vec2::new(WIDTH/2., HEIGHT/2.));
+          // canvas.draw(&line, Vec2::new(WIDTH/2., HEIGHT/2.));
+          canvas.draw(&line, Vec2::new(0., 0.));
         },
         turtle::Command::Clear => {
           // TODO: Implement clear.
@@ -179,25 +152,25 @@ impl ggez::event::EventHandler<ggez::GameError> for MainState {
     }
     canvas.finish(ctx)?;
     // Canvas::from_frame draws straight to screen.
-    // let circle = ggez::graphics::Mesh::new_circle(
+    // let circle = Mesh::new_circle(
     //   ctx,
-    //   ggez::graphics::DrawMode::fill(),
-    //   ggez::glam::Vec2::new(0.0, 0.0),
+    //   DrawMode::fill(),
+    //   Vec2::new(0.0, 0.0),
     //   100.0,
     //   2.0,
-    //   ggez::graphics::Color::WHITE,
+    //   Color::WHITE,
     // )?;
-    // canvas.draw(&circle, ggez::glam::Vec2::new(10.0, 380.0));
+    // canvas.draw(&circle, Vec2::new(10.0, 380.0));
     // canvas.finish(ctx)?;
     Ok(())
   }
 }
 
-fn main() -> ggez::GameResult {
-  let cb = ggez::ContextBuilder::new("Logo", "ggez")
-      .window_setup(ggez::conf::WindowSetup::default().title("PC Logo 4.0"))
-      .window_mode(ggez::conf::WindowMode::default().dimensions(WIDTH, HEIGHT));
+fn main() -> GameResult {
+  let cb = ContextBuilder::new("Logo", "ggez")
+      .window_setup(WindowSetup::default().title("PC Logo 4.0"))
+      .window_mode(WindowMode::default().dimensions(WIDTH, HEIGHT));
   let (ctx, event_loop) = cb.build()?;
   let state = MainState::new(&ctx)?;
-  ggez::event::run(ctx, event_loop, state)
+  event::run(ctx, event_loop, state)
 }
